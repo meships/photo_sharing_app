7cfc7de7646f21b4b781600a1f7b0f1b8555ea82
qz <meshipsmou@yahoo.co.jp>
2023-01-03 20:56:40 +0900

    別アカウントでプロフィール編集不可

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,51 @@
 class UsersController < ApplicationController
-    skip_before_action :login_required, only: [:new, :create]
-    
-    def new
-      @user = User.new
-    end
+  skip_before_action :login_required, only: [:new, :create]
+  before_action :ensure_current_user, {only: [:edit, :update]}
   
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        redirect_to user_path(@user.id)
-      else
-        render :new
-      end
-    end
+  def new
+    @user = User.new
+  end
 
-    def edit
-      @user = User.find(params[:id])
-      unless @user.id = current_user.id
-        redirect_to  new_blog_path
-      end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
     end
+  end
 
-    def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        flash[:notice] = "情報を編集しました！"
-        redirect_to blogs_path
-      else
-        render :edit
-      end
+  def edit
+    @user = User.find(params[:id])
+    unless @user.id = current_user.id
+      redirect_to  new_blog_path
     end
+  end
 
-    def show
-      @user = User.find(params[:id])
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "情報を編集しました！"
+      redirect_to blogs_path
+    else
+      render :edit
     end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
+  end
+
+  def ensure_current_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to blogs_path
     end
+  end
 end
